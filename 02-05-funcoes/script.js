24ce4f3f8f9bb8fd102a9195c221edeff9092e68
yurifws <yurifws@gmail.com>
2021-12-30 19:10:00 -0300

    funcoes

---
+++ 02-05-funcoes/script.js
@@ -0,0 +1,10 @@
+function greet (name = 'mergulhador') {
+  return 'boa noite, ' + name
+}
+
+console.log(greet('Yuri'))
+
+console.log(greet())
+
+var greeting =  greet('Matheus')
+console.log(greeting)
