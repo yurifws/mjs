77954cf34ba95e71ed589fd0ac3fdf7667e1a1bf
yurifws <yurifws@gmail.com>
2022-01-05 01:05:45 -0300

    tipando variáveis

---
+++ 09-02-tipando-variaveis/index.ts
@@ -0,0 +1,15 @@
+const personName: string = 'Yuri'
+
+const personAge: number = 27
+
+const personAlive: boolean = true
+
+const personContacts: string[] = [
+  'yuri.teste@gmail.com',
+  'yuri2.teste@gmail.com'
+]
+
+const personContacts2: (string | number)[] = [
+  0,
+  'yuri2.teste@gmail.com'
+]
